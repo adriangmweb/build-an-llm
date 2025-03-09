018bece5e883c6f762c79d424a2b550ed4327e8a
adriangmweb <adriangonzalezmesa@gmail.com>
2025-03-09 19:18:31 +0000

    add gpt dummy model

---
+++ Chapter_4/gpt2_dummy_model.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+
+# Create a dummy model
+class DummyGPTModel(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.tok_emb = nn.Embedding(config["vocab_size"], config["n_embd"])
+        self.pos_emb = nn.Embedding(config["context_length"], config["n_embd"])
+        self.dropout = nn.Dropout(config["dropout"])
+
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(config) 
+              for _ in range(config["n_layers"])]
+        )
+        self.final_norm = DummyLayerNorm(config["n_embd"])
+        self.out_head = nn.Linear(
+            config["n_embd"], config["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds
+        x = self.dropout(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+
+        return logits
+    
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+    
+class DummyLayerNorm(nn.Module):
+    # eps is a small value to avoid division by zero
+    # this one just mimic the behavior of LayerNorm
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+    
+        
---
+++ Chapter_4/main.py
@@ -0,0 +1,38 @@
+# Tokenize the text file
+import tiktoken
+import torch
+from Chapter_4.gpt2_dummy_model import DummyGPTModel
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+
+batch = torch.stack(batch, dim=0)
+print("Text batch: \n", batch)
+print()
+
+print("**** Use Dummy Model **** \n")
+
+# Set up the configuration for the model from the smallest GPT-2 model
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, # Vocabulary size
+    "context_length": 1024, # Context length
+    "n_embd": 768, # Embedding dimension
+    "n_heads": 12, # Number of attention heads
+    "n_layers": 12, # Number of layers
+    "dropout": 0.1, # Dropout rate
+    "qkv_bias": False # Query-key-value bias
+}
+
+# Initialize the model
+torch.manual_seed(123)
+
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Logits shape: \n", logits.shape)
+print("Logits: \n", logits)

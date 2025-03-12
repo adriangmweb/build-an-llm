3d6fc9551729c32e90b3987422b33cf43abb17bf
adriangmweb <adriangonzalezmesa@gmail.com>
2025-03-12 18:58:48 +0000

    layer norm

---
+++ Chapter_4/layer_norm_class.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm(nn.Module):
+    # eps is a small value to avoid division by zero
+    # this one just mimic the behavior of LayerNorm
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return norm_x * self.scale + self.shift

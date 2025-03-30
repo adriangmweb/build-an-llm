a19f6c4f496812e051871e97e90b8a76d2045edd
adriangmweb <adriangonzalezmesa@gmail.com>
2025-03-30 19:07:49 +0100

    fix import

--- Chapter_5/gpt2_weights_download.py
+++ Chapter_5/gpt2_weights_download.py
@@ -2,7 +2,7 @@
 import torch
 from Chapter_4.gpt2_models import GPTModel
 from Chapter_5.training_functions import load_weights_into_gpt
-from gpt_download import download_and_load_gpt2
+from .gpt_download import download_and_load_gpt2
 
 settings, params = download_and_load_gpt2(
     model_size="124M",
